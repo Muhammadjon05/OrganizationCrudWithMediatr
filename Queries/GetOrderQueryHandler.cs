2cab9c7087d5c328884637aa7446109a8f213b63
Muhammadjon Mahkamboyev <muhammadjonmakhkamov1@gmail.com>
2023-07-25 16:00:19 +0500

    Get And Post done

--- Queries/GetOrderQueryHandler.cs
+++ Queries/GetOrderQueryHandler.cs
@@ -1,6 +1,27 @@
-﻿namespace OrganizationCrudWithMediatr.Queries;
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using OrganizationCrudWithMediatr.DbContext;
+using OrganizationCrudWithMediatr.Mappers;
+using OrganizationCrudWithMediatr.Models;
 
-public class GetOrderQueryHandler
+namespace OrganizationCrudWithMediatr.Queries;
+
+public class GetOrderQueryHandler : IRequestHandler<GetOrdersQuery,IEnumerable<ProductModel>>
 {
-    
+    private readonly OrderDbContext _orderDbContext;
+
+    public GetOrderQueryHandler(OrderDbContext orderDbContext)
+    {
+        _orderDbContext = orderDbContext;
+    }
+
+    public async Task<IEnumerable<ProductModel>> Handle(
+        GetOrdersQuery request, 
+        CancellationToken cancellationToken)
+    {
+        var query = _orderDbContext.Products.AsQueryable();
+        var orders = await query.Select(i => i.ToModel()).ToListAsync(cancellationToken);
+        return orders;
+        
+    }
 }
